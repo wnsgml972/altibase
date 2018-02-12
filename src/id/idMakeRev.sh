cbf0f99d000dbd2c0ebf412df1e6bba256b90d87
ALTIBASE <opendev@altibase.com>
2018-02-12 16:57:01 +0900

    Fix build without Altibase internal SVN

--- src/id/idMakeRev.sh
+++ src/id/idMakeRev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-LC_ALL=C svn info $ALTIBASE_DEV/src/ | grep URL | gawk '{print "#define ALTIBASE_SVN_URL \"" $2 "\""}'
-LC_ALL=C svn info $ALTIBASE_DEV/src/ | grep Revision | gawk '{print "#define ALTIBASE_SVN_REVISION " $2}'
+echo '#define ALTIBASE_SVN_URL ""'
+echo '#define ALTIBASE_SVN_REVISION 0'
